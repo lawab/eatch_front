7e1895095467401ba442280ed6d43bf49bb91e4c
Hans Ndong <hansndong@Hanss-MacBook-Pro.local>
2023-05-12 11:24:32 +0100

    Mise à jour du 12-05-2023

--- eatch/lib/main.dart
+++ eatch/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MatiereAffiche(), //Authentification(), //const Authentification(),
+      home: Authentification(), //const Authentification(),
     );
   }
 }
